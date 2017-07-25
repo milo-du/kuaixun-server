7dbe6e137f68e9c3f1f0c2020e899a00fe8e105d
milo-du <milodu@foxmail.com>
2017-07-25 20:31:14 +0800

    init

--- application/controllers/AdminJobConfig.php
+++ application/controllers/AdminJobConfig.php
@@ -16,4 +16,35 @@ class AdminJobConfig extends BaseController
         $this->result['data'] = $result;
         $this->jsonOutput();
     }
+
+    function setAdminJobConfig()
+    {
+        $this->isAdminLogin();
+        $this->load->model('Admin_job_config_model');
+        $uid = $this->input->get('uid');
+        $publisherPrice = $this->input->post('publisherPrice');
+        $brusherPrice = $this->input->post('brusherPrice');
+        if (strlen($publisherPrice) == 0) {
+            $result = array('ret' => -1, 'msg' => '发布者价钱不能为空');
+            $this->result = $result;
+            $this->jsonOutput();
+            return;
+        }
+        if (strlen($brusherPrice) == 0) {
+            $result = array('ret' => -1, 'msg' => '刷手价钱不能为空');
+            $this->result = $result;
+            $this->jsonOutput();
+            return;
+        }
+        $data = $this->Admin_job_config_model->setAdminJobConfig($publisherPrice, $brusherPrice, $uid);
+        if ($data) {
+            $result = array('ret' => 0, 'msg' => '设置成功');
+            $this->result = $result;
+            $this->jsonOutput();
+        } else {
+            $result = array('ret' => -1, 'msg' => '设置失败，请稍候重试');
+            $this->result = $result;
+            $this->jsonOutput();
+        }
+    }
 }
--- application/models/Admin_job_config_model.php
+++ application/models/Admin_job_config_model.php
@@ -7,10 +7,10 @@ class Admin_job_config_model extends CI_Model
         $sql = 'SELECT * FROM admin_job_config order by createTime limit 0,1';
         $query = $this->db->query($sql);
         return $query->result();
-    }
+   }
 
-    public function setAdminJobConfig($publisherPrice,$bursherPrice){
-        $sql = "INSERT INTO admin_job_config(publisherPrice,brusherPrice,regIP,lastLoginIP) VALUES(" . $this->db->escape($phone) . "," . $this->db->escape($pwd) . "," . $this->db->escape($ip) . "," . $this->db->escape($ip) . ")";
+    public function setAdminJobConfig($publisherPrice,$bursherPrice,$uid){
+        $sql = "INSERT INTO admin_job_config(publisherPrice,brusherPrice,uid) VALUES(" . $this->db->escape($publisherPrice) . "," . $this->db->escape($bursherPrice) . "," . $this->db->escape($uid) . ")";
         return $this->db->simple_query($sql);
     }
 }

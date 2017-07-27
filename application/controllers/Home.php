9249e2bd9a9905023a356c36aaab56b19a04f181
杜铭 <milodu@foxmail.com>
2017-07-27 16:25:03 +0800

    fix

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -22,32 +22,6 @@ class home extends BaseController
      */
     public function index()
     {
-        //需要登录
-        $this->isLogin();
-        var_dump($this->router->fetch_directory());
-        var_dump($this->router->fetch_class());
-        var_dump($this->router->fetch_method());
-        $aaa = $this->input->get('id');
-        $arr = array('aa' => $aaa);
-        $this->result = $arr;
         $this->jsonOutput();
-    }
-
-    public function login()
-    {
-        $userId = $this->input->get('userName');
-        $passwd = $this->input->get('passwd');
-        //查找用户表的的用户信息
-        $dbPasswd = 'bfb7ca0181b66c21ea77edec46f2d7fa';
-        $userName = 'test';
-        echo md5('duming' . $passwd);
-        if ($dbPasswd == md5('duming' . $passwd)) {
-            $_SESSION['token'] = md5('duming' . $userId);
-            $this->jsonOutput();
-        } else {
-            $this->result['ret'] = 2002;
-            $this->result['msg'] = '用户名或者密码不正确';
-            $this->jsonOutput();
-        }
     }
 }

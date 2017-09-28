a838ee05f8cc176a3bd5602862c1ed52e0482c0e
milo-du <milodu@foxmail.com>
2017-09-29 00:22:15 +0800

    fix

--- application/models/Admin_readonly_model.php
+++ application/models/Admin_readonly_model.php
@@ -4,7 +4,16 @@ class Admin_readonly_model extends CI_Model
 {
     public function setReadOnly($uid,$money,$Provider,$Count)
     {
-        $sql = "UPDATE readonly SET money=?,count=?,provider=? where brusherUserID=?";
-        return $this->db->query($sql, array($money,$Count,$Provider,$uid));
+        $sql1 = "SELECT * FROM readonly where brusherUserID=?";
+        $query = $this->db->query($sql1, array($uid));
+        $data = $query->result_array();
+        if(count($data)>0)
+        {
+            $sql2 = "UPDATE readonly SET money=?,count=?,provider=? where brusherUserID=?";
+        }
+        else{
+            $sql2 = "insert readonly(money,count,provider,brusherUserID) values(?,?,?,?)";
+        }
+        return $this->db->query($sql2, array($money,$Count,$Provider,$uid));
     }
 }
